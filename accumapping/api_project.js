de2155d72e3f14fe4655eb00bba45ac9e5be7d8f
JakevinLo <42030549+KevinAccupass@users.noreply.github.com>
2020-07-29 15:31:00 +0800

    更新資料

--- accumapping/api_project.js
+++ accumapping/api_project.js
@@ -3,13 +3,13 @@ define({
   "version": "0.1.0",
   "description": "配對功能所有Api",
   "title": "配對功能所有Api",
-  "url": "https://accu-mapping-vvr3kr7cwa-de.a.run.app/mapping/user",
+  "url": "https://BASE_URL/mapping/user",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-07-29T06:54:02.581Z",
+    "time": "2020-07-29T07:30:04.892Z",
     "url": "http://apidocjs.com",
     "version": "0.23.0"
   }
